fbee1ee584abe8ea389a839db0c3e77a95977360
jobchong <jobchong@gmail.com>
2016-02-22 21:20:59 +0800

    Grep all files instead of just first instance

--- grep.py
+++ grep.py
@@ -2,6 +2,7 @@ import re
 import os.path
 import string
 from bs4 import BeautifulSoup
+from os import listdir
 
 topdir = "."
 exten = ".html"
@@ -15,15 +16,10 @@ For judgement paragraphs, type Judg-1.
 searchTerm = raw_input("Which paragraphs do you want to search for? ")
 
 
-def step (ext, dirname, names):
-    ext = ext.lower()
-
-    for name in names:
-        if name.lower().endswith(ext):
-            with open(os.path.join(dirname, name)) as currentCase:
+with open("/Users/jobchong/git/scrapelawnet/Output.txt", "w") as f:
+    for filename in listdir("/Users/jobchong/git/scrapelawnet/"):
+        if filename.endswith(".html"):
+            with open("/Users/jobchong/git/scrapelawnet/" + filename) as currentCase:
                 soup = BeautifulSoup(currentCase, "html.parser")
                 facts = soup.find_all("p", class_ = searchTerm)
-                f = open("Output.txt", "w")
                 f.write(str(facts))
-                
-os.path.walk(topdir, step, exten)

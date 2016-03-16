23fbe5f8a9eed9a61b20e5c007dadc933bae1955
kingmagikarp <wu.guowei@vjc.sg>
2016-03-16 09:19:22 +0800

    Update general_extractor.py
    added court retrieve

--- general_extractor.py
+++ general_extractor.py
@@ -1,26 +1,39 @@
 from os import listdir
 import string
-
+import re 
 
 casefolder = "/Users/wuguowei/Google Drive/Year 4 Sem 2/Artificial Intelligence/scrapelawnet/casesTXT/"
 datafolder = "/Users/wuguowei/Google Drive/Year 4 Sem 2/Artificial Intelligence/scrapelawnet/data/"
 
 #extracting level of court
-
+#list of headers
+headerlist = []                     
+#counter
 n=0
-with open(datafolder+"/data_court.txt", "w") as f:                              #opens a datafile
+with open(datafolder+"data_court.txt", "w") as f:                              #opens a datafile
     for filename in listdir(casefolder):                                        #calling a txt file
-        if filename.endswith(".txt"):       
+        if filename.endswith(".txt") and not filename.startswith(".DS"):      
             with open(casefolder + filename) as currentCase:                    
-                lines = currentCase.readlines()
-                courtdata = string.strip(string.lstrip(lines[4],"Tribunal/Court:" + "Tribunal/Court : "))
+                lines = currentCase.readlines()                                 #linify case
+                
+                courtdata = string.strip(string.strip(lines[4],"Tribunal/Court:" + "Tribunal/Court : "))        ###COURT RETRIEVE
                 if courtdata == "High Court":
-                    result = "H"
+                    courtresult = "H"
                 elif courtdata == "District Court": 
-                    result = "D"
+                    courtresult = "D"
+                elif courtdata == "Magistrates Court":
+                    courtrusult = "M"
                 else:
-                    result = "NA"
-                f.write(str(n)+","+result+"\n")
+                    courtresult = "NA"
+               
+                coramdata = lines[5].replace("Coram: ", "", 1)                                                  ### CORAM RETRIEVE
+                coramdata = coramdata.replace(" Coram :  ", "", 1)
+                
+                
+                
+                ##continue to add the headers
+                f.write(str(n)+","+courtresult+","+coramdata + "\n")
+                print filename
                 n += 1
                 
                 
